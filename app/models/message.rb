8bd9e56d21c164b7c0d39764deb3176148b4b6a2
Paul Unger <g.paul.u@gmail.com>
2013-04-27 15:52:19 -0500

    Added some validations to the message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
   attr_accessible :associated_images, :body, :from, :title, :to
+  
+  validates :body, :presence => true
+  validates :from, :presence => true
+  validates :to, :presence => true
 end
