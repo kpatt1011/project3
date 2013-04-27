8967e200b062de3efcd73a0fb012a41f85712712
kpatt1011 <keith.pattison.tamu@gmail.com>
2013-04-27 16:44:36 -0500

    Defined the migration for the relationship between user and messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
+  belongs_to :user
   attr_accessible :associated_images, :body, :from, :title, :to
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+
+  has_many :messages
+
   attr_accessible :group, :is_admin, :key, :name
 end
