f74ad7120f21ac54df83f5f7a55c9f47dd4f22e1
Paul Unger <g.paul.u@gmail.com>
2013-04-27 16:15:55 -0500

    Added validations to group and user models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ActiveRecord::Base
   attr_accessible :admin_key, :location, :messages, :name, :users
+  
+  validates :admin_key, :presence => true
+  validates :name, :presence => true, :uniqueness => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :group, :is_admin, :key, :name
+  
+  validates :key, :presence => true, :uniqueness => true
+  validates :name, :presence => true
 end
