a26acf5dedf79378f54852fdc89b883288982833
Paul Unger <g.paul.u@gmail.com>
2013-05-06 15:40:07 -0500

    Facebook login now goes to facebook. Still getting errors with callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['136999566485816'], ENV['ea7bcffd7a8b3844202b227bcf97bef9'],
+  provider :facebook, '136999566485816', 'ea7bcffd7a8b3844202b227bcf97bef9',
            :scope => 'email,user_location,user_photos'
 end
