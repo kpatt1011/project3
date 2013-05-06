298970b1c61619595152272a7da16a6a4f3034a8
kpatt1011 <keith.pattison.tamu@gmail.com>
2013-05-06 13:12:47 -0500

    Currently working on the page views and controllers. There are errors right now however

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Project3::Application.routes.draw do
   match 'messages/groups' => 'groups#index'
 
   match 'pages_editNiceThings' => 'pages#editNiceThings'
+  match 'pages_viewGroup' => 'pages#viewGroup'
+  match 'pages_joinGroup' => 'pages#joinGroup'
   match 'home' => 'pages#index'
 
 
